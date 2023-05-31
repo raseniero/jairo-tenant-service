84ca4b4bec320dd263381a64deacc3df671ed6c6
Ramon Aseniero <raseniero@gmail.com>
2023-05-30 17:49:51 -0700

    added new fields

--- users/serializers.py
+++ users/serializers.py
@@ -2,11 +2,17 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="user-detail", format="html"
+    )
+
     class Meta:
         model = User
         fields = (
+            "url",
             "id",
+            "highlight",
             "username",
             "password",
             "email",
