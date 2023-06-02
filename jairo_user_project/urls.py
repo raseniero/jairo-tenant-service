fffaebf37d095f836612873513da69e5b373ab91
Ramon Aseniero <raseniero@gmail.com>
2023-06-01 21:15:02 -0700

    remove unused import

--- jairo_user_project/urls.py
+++ jairo_user_project/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import api_root
 
 urlpatterns = [
     path("admin/", admin.site.urls),
