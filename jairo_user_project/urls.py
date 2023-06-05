192db583acf3e059cf497c43db217073ea4ce733
ramon <raseniero@gmail.com>
2023-06-05 01:04:09 -0700

    added static to the url

--- jairo_user_project/urls.py
+++ jairo_user_project/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.api_root),
     path("", include("user.urls")),
     path("", include("person.urls")),  # new
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
